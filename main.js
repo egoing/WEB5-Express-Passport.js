5094046499287f1fb6296518e615c05b3a51418c
egoing <egoing@gmail.com>
2018-06-30 23:45:52 +0900

    4.홈페이지 구현

--- main.js
+++ main.js
@@ -1,10 +1,21 @@
 var express = require('express')
 var app = express()
+var fs = require('fs');
+var template = require('./lib/template.js');
 
 //route, routing
 //app.get('/', (req, res) => res.send('Hello World!'))
-app.get('/', function(req, res) { 
-  return res.send('/');
+app.get('/', function(request, response) { 
+  fs.readdir('./data', function(error, filelist){
+    var title = 'Welcome';
+    var description = 'Hello, Node.js';
+    var list = template.list(filelist);
+    var html = template.HTML(title, list,
+      `<h2>${title}</h2>${description}`,
+      `<a href="/create">create</a>`
+    ); 
+    response.send(html);
+  });
 });
 
 app.get('/page', function(req, res) { 
