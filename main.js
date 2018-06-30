b939f4e2d87befda7e4bd0d8203c2c787b9ff31e
egoing <egoing@gmail.com>
2018-06-30 23:24:56 +0900

    3.2. Hello world 2

--- main.js
+++ main.js
@@ -1,9 +1,19 @@
-const express = require('express')
-const app = express()
+var express = require('express')
+var app = express()
 
-app.get('/', (req, res) => res.send('Hello World!'))
+//route, routing
+//app.get('/', (req, res) => res.send('Hello World!'))
+app.get('/', function(req, res) { 
+  return res.send('/');
+});
 
-app.listen(3000, () => console.log('Example app listening on port 3000!'))
+app.get('/page', function(req, res) { 
+  return res.send('/page');
+});
+
+app.listen(3000, function() {
+  console.log('Example app listening on port 3000!')
+});
 
 /*
 var http = require('http');
