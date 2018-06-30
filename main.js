549db5253bfe4d3d1833aa515546abd9c431d724
egoing <egoing@gmail.com>
2018-07-01 00:04:02 +0900

    5.2.상세페이지구현 2

--- main.js
+++ main.js
@@ -1,6 +1,8 @@
 var express = require('express')
 var app = express()
 var fs = require('fs');
+var path = require('path');
+var sanitizeHtml = require('sanitize-html');
 var template = require('./lib/template.js');
 
 //route, routing
@@ -19,7 +21,27 @@ app.get('/', function(request, response) {
 });
 
 app.get('/page/:pageId', function(request, response) { 
-  response.send(request.params);
+  fs.readdir('./data', function(error, filelist){
+    var filteredId = path.parse(request.params.pageId).base;
+    fs.readFile(`data/${filteredId}`, 'utf8', function(err, description){
+      var title = request.params.pageId;
+      var sanitizedTitle = sanitizeHtml(title);
+      var sanitizedDescription = sanitizeHtml(description, {
+        allowedTags:['h1']
+      });
+      var list = template.list(filelist);
+      var html = template.HTML(sanitizedTitle, list,
+        `<h2>${sanitizedTitle}</h2>${sanitizedDescription}`,
+        ` <a href="/create">create</a>
+          <a href="/update?id=${sanitizedTitle}">update</a>
+          <form action="delete_process" method="post">
+            <input type="hidden" name="id" value="${sanitizedTitle}">
+            <input type="submit" value="delete">
+          </form>`
+      );
+      response.send(html);
+    });
+  });
 });
 
 app.listen(3000, function() {
@@ -41,17 +63,6 @@ var app = http.createServer(function(request,response){
     var pathname = url.parse(_url, true).pathname;
     if(pathname === '/'){
       if(queryData.id === undefined){
-        fs.readdir('./data', function(error, filelist){
-          var title = 'Welcome';
-          var description = 'Hello, Node.js';
-          var list = template.list(filelist);
-          var html = template.HTML(title, list,
-            `<h2>${title}</h2>${description}`,
-            `<a href="/create">create</a>`
-          ); 
-          response.writeHead(200);
-          response.end(html);
-        });
       } else {
         fs.readdir('./data', function(error, filelist){
           var filteredId = path.parse(queryData.id).base;
