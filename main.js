3680648c5b8de523a1056709b716a13f907174bd
egoing <egoing@gmail.com>
2018-07-01 00:48:01 +0900

    9.페이지 삭제 구현

--- main.js
+++ main.js
@@ -35,7 +35,7 @@ app.get('/page/:pageId', function(request, response) {
         `<h2>${sanitizedTitle}</h2>${sanitizedDescription}`,
         ` <a href="/create">create</a>
           <a href="/update/${sanitizedTitle}">update</a>
-          <form action="delete_process" method="post">
+          <form action="/delete_process" method="post">
             <input type="hidden" name="id" value="${sanitizedTitle}">
             <input type="submit" value="delete">
           </form>`
@@ -118,10 +118,24 @@ app.post('/update_process', function(request, response){
       var description = post.description;
       fs.rename(`data/${id}`, `data/${title}`, function(error){
         fs.writeFile(`data/${title}`, description, 'utf8', function(err){
-          response.writeHead(302, {Location: `/?id=${title}`});
-          response.end();
+          response.redirect(`/?id=${title}`);
         })
       });
+  });
+});
+
+app.post('/delete_process', function(request, response){
+  var body = '';
+  request.on('data', function(data){
+      body = body + data;
+  });
+  request.on('end', function(){
+      var post = qs.parse(body);
+      var id = post.id;
+      var filteredId = path.parse(id).base;
+      fs.unlink(`data/${filteredId}`, function(error){
+        response.redirect('/');
+      })
   });
 });
 
@@ -154,19 +168,7 @@ var app = http.createServer(function(request,response){
     } else if(pathname === '/update_process'){
       
     } else if(pathname === '/delete_process'){
-      var body = '';
-      request.on('data', function(data){
-          body = body + data;
-      });
-      request.on('end', function(){
-          var post = qs.parse(body);
-          var id = post.id;
-          var filteredId = path.parse(id).base;
-          fs.unlink(`data/${filteredId}`, function(error){
-            response.writeHead(302, {Location: `/`});
-            response.end();
-          })
-      });
+      
     } else {
       response.writeHead(404);
       response.end('Not found');
