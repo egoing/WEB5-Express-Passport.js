8898c9924d70b1c2c427b76b890a2b75d4c20d02
egoing <egoing@gmail.com>
2018-07-01 23:53:24 +0900

    14.3.라우터 - 파일로 분리 - index

--- main.js
+++ main.js
@@ -1,12 +1,10 @@
 var express = require('express');
 var app = express();
 var fs = require('fs');
-var path = require('path');
-var qs = require('querystring');
 var bodyParser = require('body-parser');
-var sanitizeHtml = require('sanitize-html');
 var compression = require('compression')
-var template = require('./lib/template.js');
+
+var indexRouter = require('./routes/index');
 var topicRouter = require('./routes/topic');
 
 app.use(express.static('public'));
@@ -19,33 +17,8 @@ app.get('*', function(request, response, next){
   });
 });
 
+app.use('/', indexRouter);
 app.use('/topic', topicRouter);
-
-
-//route, routing
-//app.get('/', (req, res) => res.send('Hello World!'))
-app.get('/', function(request, response) { 
-  var title = 'Welcome';
-  var description = 'Hello, Node.js';
-  var list = template.list(request.list);
-  var html = template.HTML(title, list,
-    `
-    <h2>${title}</h2>${description}
-    <img src="/images/hello.jpg" style="width:300px; display:block; margin-top:10px;">
-    `,
-    `<a href="/topic/create">create</a>`
-  ); 
-  response.send(html);
-});
-
-
-
-
-
-
-
-
-
 
 app.use(function(req, res, next) {
   res.status(404).send('Sorry cant find that!');
---
+++ routes/index.js
@@ -0,0 +1,19 @@
+var express = require('express');
+var router = express.Router();
+var template = require('../lib/template.js');
+
+router.get('/', function(request, response) { 
+    var title = 'Welcome';
+    var description = 'Hello, Node.js';
+    var list = template.list(request.list);
+    var html = template.HTML(title, list,
+      `
+      <h2>${title}</h2>${description}
+      <img src="/images/hello.jpg" style="width:300px; display:block; margin-top:10px;">
+      `,
+      `<a href="/topic/create">create</a>`
+    ); 
+    response.send(html);
+  });
+  
+  module.exports = router;
