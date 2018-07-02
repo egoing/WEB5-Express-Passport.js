f62ddc42fbe603d64d573c8ef809703b1a320b78
egoing <egoing@gmail.com>
2018-07-02 10:05:36 +0900

    소스코드 정리

--- main.js
+++ main.js
@@ -34,37 +34,3 @@ app.use(function (err, req, res, next) {
 app.listen(3000, function() {
   console.log('Example app listening on port 3000!')
 });
-
-/*
-var http = require('http');
-var fs = require('fs');
-var url = require('url');
-var qs = require('querystring');
-var template = require('./lib/template.js');
-var path = require('path');
-var sanitizeHtml = require('sanitize-html');
-
-var app = http.createServer(function(request,response){
-    var _url = request.url;
-    var queryData = url.parse(_url, true).query;
-    var pathname = url.parse(_url, true).pathname;
-    if(pathname === '/'){
-      if(queryData.id === undefined){
-      } else {
-      }
-    } else if(pathname === '/create'){
-    } else if(pathname === '/create_process'){
-      
-    } else if(pathname === '/update'){
-      
-    } else if(pathname === '/update_process'){
-      
-    } else if(pathname === '/delete_process'){
-      
-    } else {
-      response.writeHead(404);
-      response.end('Not found');
-    }
-});
-app.listen(3000);
-*/
