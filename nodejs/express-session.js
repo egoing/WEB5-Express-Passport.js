bc5db9fb016e45c7f334bbac3ce1f52c1151d6ef
egoing <egoing@gmail.com>
2018-07-29 02:00:07 +0900

    2.2.express-session의 이해

--- nodejs/express-session.js
+++ nodejs/express-session.js
@@ -1,19 +1,25 @@
 var express = require('express')
 var parseurl = require('parseurl')
 var session = require('express-session')
- 
+
 var app = express()
- 
+
 app.use(session({
-  secret: 'asadlfkj!@#!@#dfgasdg',
-  resave: false,
-  saveUninitialized: true
+    secret: 'asadlfkj!@#!@#dfgasdg',
+    resave: false,
+    saveUninitialized: true
 }))
- 
+
 app.get('/', function (req, res, next) {
-  res.send('Hello session');
+    console.log(req.session);
+    if(req.session.num === undefined){
+        req.session.num = 1;
+    } else {
+        req.session.num =  req.session.num + 1;
+    }
+    res.send(`Views : ${req.session.num}`);
 })
 
-app.listen(3000, function(){
+app.listen(3000, function () {
     console.log('3000!');
 });
