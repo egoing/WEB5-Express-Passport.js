0b7cd60f75e49944538c4e1009a285759b221014
egoing <egoing@gmail.com>
2018-07-29 10:15:02 +0900

    6.4.인증구현-인증상태를 UI에 반영

--- routes/index.js
+++ routes/index.js
@@ -2,18 +2,35 @@ var express = require('express');
 var router = express.Router();
 var template = require('../lib/template.js');
 
-router.get('/', function(request, response) { 
-    var title = 'Welcome';
-    var description = 'Hello, Node.js';
-    var list = template.list(request.list);
-    var html = template.HTML(title, list,
-      `
+function authIsOwner(request, response) {
+  if (request.session.is_logined) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+function authStatusUI(request, response){
+  var authStatusUI = '<a href="/auth/login">login</a>'
+  if(authIsOwner(request, response)){
+    authStatusUI = `${request.session.nickname} | <a href="/auth/logout">logout</a>`;
+  }
+  return authStatusUI;
+}
+
+router.get('/', function (request, response) {
+  var title = 'Welcome';
+  var description = 'Hello, Node.js';
+  var list = template.list(request.list);
+  var html = template.HTML(title, list,
+    `
       <h2>${title}</h2>${description}
       <img src="/images/hello.jpg" style="width:300px; display:block; margin-top:10px;">
       `,
-      `<a href="/topic/create">create</a>`
-    ); 
-    response.send(html);
-  });
-  
-  module.exports = router;
+    `<a href="/topic/create">create</a>`,
+    authStatusUI(request, response)
+  );
+  response.send(html);
+});
+
+module.exports = router;
