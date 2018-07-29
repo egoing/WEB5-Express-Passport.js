c4b5312aa9012048b34f6709075abc6795928bf8
egoing <egoing@gmail.com>
2018-07-29 23:08:57 +0900

    6.8.인증구현-세션저장

--- routes/auth.js
+++ routes/auth.js
@@ -33,7 +33,9 @@ router.post('/login_process', function (request, response) {
   if(email === authData.email && password === authData.password){
     request.session.is_logined = true;
     request.session.nickname = authData.nickname;
-    response.redirect(`/`);
+    request.session.save(function(){
+      response.redirect(`/`);
+    });
   } else {
     response.send('Who?');
   }
@@ -44,104 +46,5 @@ router.get('/logout', function (request, response) {
     response.redirect('/');
   });
 });
-
-/*
-router.get('/create', function(request, response){
-    var title = 'WEB - create';
-    var list = template.list(request.list);
-    var html = template.HTML(title, list, `
-      <form action="/topic/create_process" method="post">
-        <p><input type="text" name="title" placeholder="title"></p>
-        <p>
-          <textarea name="description" placeholder="description"></textarea>
-        </p>
-        <p>
-          <input type="submit">
-        </p>
-      </form>
-    `, '');
-    response.send(html);
-  });
-  
-  router.post('/create_process', function(request, response){
-    var post = request.body;
-    var title = post.title;
-    var description = post.description;
-    fs.writeFile(`data/${title}`, description, 'utf8', function(err){
-      response.redirect(`/topic/${title}`);
-    });
-  });
-  
-  router.get('/update/:pageId', function(request, response){
-    var filteredId = path.parse(request.params.pageId).base;
-    fs.readFile(`data/${filteredId}`, 'utf8', function(err, description){
-      var title = request.params.pageId;
-      var list = template.list(request.list);
-      var html = template.HTML(title, list,
-        `
-        <form action="/topic/update_process" method="post">
-          <input type="hidden" name="id" value="${title}">
-          <p><input type="text" name="title" placeholder="title" value="${title}"></p>
-          <p>
-            <textarea name="description" placeholder="description">${description}</textarea>
-          </p>
-          <p>
-            <input type="submit">
-          </p>
-        </form>
-        `,
-        `<a href="/topic/create">create</a> <a href="/topic/update/${title}">update</a>`
-      );
-      response.send(html);
-    });
-  });
-  
-  router.post('/update_process', function(request, response){
-    var post = request.body;
-    var id = post.id;
-    var title = post.title;
-    var description = post.description;
-    fs.rename(`data/${id}`, `data/${title}`, function(error){
-      fs.writeFile(`data/${title}`, description, 'utf8', function(err){
-        response.redirect(`/topic/${title}`);
-      })
-    });
-  });
-  
-  router.post('/delete_process', function(request, response){
-    var post = request.body;
-    var id = post.id;
-    var filteredId = path.parse(id).base;
-    fs.unlink(`data/${filteredId}`, function(error){
-      response.redirect('/');
-    });
-  });
-  
-  router.get('/:pageId', function(request, response, next) { 
-    var filteredId = path.parse(request.params.pageId).base;
-    fs.readFile(`data/${filteredId}`, 'utf8', function(err, description){
-      if(err){
-        next(err);
-      } else {
-        var title = request.params.pageId;
-        var sanitizedTitle = sanitizeHtml(title);
-        var sanitizedDescription = sanitizeHtml(description, {
-          allowedTags:['h1']
-        });
-        var list = template.list(request.list);
-        var html = template.HTML(sanitizedTitle, list,
-          `<h2>${sanitizedTitle}</h2>${sanitizedDescription}`,
-          ` <a href="/topic/create">create</a>
-            <a href="/topic/update/${sanitizedTitle}">update</a>
-            <form action="/topic/delete_process" method="post">
-              <input type="hidden" name="id" value="${sanitizedTitle}">
-              <input type="submit" value="delete">
-            </form>`
-        );
-        response.send(html);
-      }
-    });
-  });
-  */
 
 module.exports = router;
