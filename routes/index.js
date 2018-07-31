89e782f5b4e4d20e0ae2048512e6d20b9f51cdac
egoing <egoing@gmail.com>
2018-07-31 19:36:42 +0900

    6.passport 로그인 확인

--- routes/index.js
+++ routes/index.js
@@ -5,6 +5,7 @@ var auth = require('../lib/auth');
 
 router.get('/', function (request, response) {
   console.log('/', request.user);
+  
   var title = 'Welcome';
   var description = 'Hello, Node.js';
   var list = template.list(request.list);
